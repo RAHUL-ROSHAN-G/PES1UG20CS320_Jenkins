7a418da911c2a1e60e8d2e042a96d2f9ac246e49
RAHUL-ROSHAN-G <rahulroshanganesh2002@gmail.com>
2023-02-15 20:45:15 +0530

    Updated

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, World from PES1UG20CS320\n";
+  cout << "Hello, Jenkins and Github\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
